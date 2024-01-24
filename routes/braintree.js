a1b400fc051887214ead04f5438518273b97fb58
Adedeji-Kehinde <129548218+Adedeji-Kehinde@users.noreply.github.com>
2024-01-24 10:48:52 +0000

    updated

--- routes/braintree.js
+++
@@ -1,8 +0,0 @@
-const braintree = require("braintree");
-
-const gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
-  merchantId: "6hnmdjfxhrywhbkb",
-  publicKey: "v8czpcqrmp49nd63",
-  privateKey: "5bc8af5523d28048d1cdfe2651dac200"
-});
--- routes/checkout.js
+++ routes/checkout.js
@@ -3,15 +3,22 @@ const express = require('express');
 const router = express.Router();
 const braintree = require('braintree');
 
-router.post('/', (req, res, next) => {
+
   const gateway = new braintree.BraintreeGateway({
     environment: braintree.Environment.Sandbox,
+    
     // Use your own credentials from the sandbox Control Panel here
     merchantId: '6hnmdjfxhrywhbkb',
     publicKey: 'v8czpcqrmp49nd63',
     privateKey: '5bc8af5523d28048d1cdfe2651dac200'
   });
 
+  router.get("/", (req, res) => {
+    gateway.clientToken.generate({}, (err, response) => {
+      res.send(response.clientToken);
+    });
+  });
+  router.post('/', (req, res, next) => {
   // Use the payment method nonce here
   const nonceFromTheClient = req.body.paymentMethodNonce;
   // Create a new transaction for $10
